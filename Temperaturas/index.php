c4da437b303ae496c10fc53bc4ec6d0d17fd09d0
Javi <jruiztoledog@gmail.com>
2015-10-04 23:40:08 +0200

    Modificado index de Temperaturas

--- Temperaturas/index.php
+++ Temperaturas/index.php
@@ -1,9 +1,4 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
@@ -11,7 +6,25 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        // put your code here
+        $ciudades = ["Madrid", "Barcelona", "Sevilla", "Bilbao"];
+        $meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+        $temps = ["Temp max", "Temp mín"];
         ?>
+        <form action="temperaturas.php" method="POST">
+            <?php
+                    foreach ($ciudades as $ciudad) {
+                        echo "<table><tr><th colspan=3 name=\"temp[$ciudad]\">$ciudad</th></tr>";
+                        echo "<tr><td>Mes</td><td>Temp m&aacutexima</td><td>Temp m&iacutenima</td>";
+                        foreach ($meses as $mes) {
+                            echo "<tr><td name=\"temp[$ciudad][$mes]\">" + $mes + "</td>";
+                            foreach ($temps as $temp) {
+                                echo "<td><input type=\"text\" name=\"temp[$ciudad][$mes][$temps]\">" + $temp + "</td></tr>";
+                            }
+                        }
+                        echo "</table>";
+                    }
+            ?>
+            <input type="SUBMIT" value="Enviar">
+        </form>
     </body>
 </html>
