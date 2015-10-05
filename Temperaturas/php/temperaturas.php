a35a0fbeba28e88c700fc94a5f3f6e384fa4730f
2daw <2daw@A101PC1>
2015-10-05 20:35:14 +0200

    Actualizado

--- Temperaturas/php/temperaturas.php
+++ Temperaturas/php/temperaturas.php
@@ -11,12 +11,18 @@ $ciudades = $temps['ciudad'];
 $meses = $temps['ciudad']['mes'];
 $temperaturas = $temps['ciudad']['mes']['temperaturas'];
     
-foreach ($ciudades as $ciudad => $mes) {
-    echo "$ciudad elll $mes";
-}
-
-
-
+foreach ($ciudades as $ciudad) {
+                        echo "<table><tr><th colspan=3>$ciudad</th></tr>";
+                        echo "<tr class=\"tit\"><td class=\"dcha\">Mes</td><td>Tm&aacutex</td><td>Tm&iacuten</td></tr>";
+                        foreach ($meses as $mes) {
+                            echo "<tr><td class=\"dcha\">$mes</td>";
+                            foreach ($temps as $temperaturas) {
+                                echo "<td><input type=\"text\" class=\"lil\"name=\"temp[$ciudad][$mes][$temperaturas]\"></td>";
+                            }
+                            echo "</tr>";
+                        }
+                        echo "</table>";
+                    }
 ?>
 
             </body>
