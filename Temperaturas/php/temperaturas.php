cdc85bb0579674dd4909037bf2c1b854dd1c00f6
2daw <2daw@A101PC1>
2015-10-05 20:47:15 +0200

    Actualizado

--- Temperaturas/php/temperaturas.php
+++ Temperaturas/php/temperaturas.php
@@ -7,22 +7,24 @@
 
 $temps = $_POST['temp'];
 
-$ciudades = $temps['ciudad'];
-$meses = $temps['ciudad']['mes'];
+/*$ciudades = $temps['ciudad'];
+$meses = $temps['ciudad']['mes'];*/
 $temperaturas = $temps['ciudad']['mes']['temperaturas'];
     
-foreach ($ciudades as $ciudad) {
-                        echo "<table><tr><th colspan=3>$ciudad</th></tr>";
-                        echo "<tr class=\"tit\"><td class=\"dcha\">Mes</td><td>Tm&aacutex</td><td>Tm&iacuten</td></tr>";
-                        foreach ($meses as $mes) {
-                            echo "<tr><td class=\"dcha\">$mes</td>";
-                            foreach ($temps as $temperaturas) {
-                                echo "<td><input type=\"text\" class=\"lil\"name=\"temp[$ciudad][$mes][$temperaturas]\"></td>";
-                            }
-                            echo "</tr>";
-                        }
-                        echo "</table>";
-                    }
+foreach ($temperaturas as $temp => $meses) {
+    echo "$temp $meses";
+    //echo "$temperaturas[ciudad] asdasdf $temperaturas[meses]";
+    /*echo "<table><tr><th colspan=3>$ciudad</th></tr>";
+    echo "<tr class=\"tit\"><td class=\"dcha\">Mes</td><td>Tm&aacutex</td><td>Tm&iacuten</td></tr>";
+    foreach ($meses as $mes) {
+        echo "<tr><td class=\"dcha\">$mes</td>";
+        foreach ($temps as $temperaturas) {
+            echo "<td><input type=\"text\" class=\"lil\"name=\"temp[$ciudad][$mes][$temperaturas]\"></td>";
+        }
+        echo "</tr>";
+    }
+    echo "</table>";*/
+}
 ?>
 
             </body>
