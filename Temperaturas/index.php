f2639365732ee69f0acf1d3ad2df43d1fd9251e7
2daw <2daw@A101PC1>
2015-10-05 20:16:12 +0200

    misco

--- Temperaturas/index.php
+++ Temperaturas/index.php
@@ -5,16 +5,22 @@
         <title>Temperaturas</title>
         <style>
             table{text-align: center;
-                    width: 20%;
                     float: left;
-                    margin: 30px;
+                    margin: 20px;
                     border: 1px solid goldenrod;}
+            
             tr{border: 1px solid goldenrod;}
+            
             .dcha{text-align: right;
                     font-weight: bold;}
+            
             .tit{font-weight: bold;}
+            
             .centrado{text-align: center;
-                        width: 100%;}
+                        clear: both;}
+            
+            .lil{width: 35px;}
+                        
         </style>
     </head>
     <body>
@@ -23,22 +29,22 @@
         $meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
         $temps = ["TempMax", "TempMín"];
         ?>
-        <form action="temperaturas.php" method="POST">
+        <form action="php/temperaturas.php" method="POST">
             <?php
                     foreach ($ciudades as $ciudad) {
-                        echo "<table><tr><th colspan=3 name=\"temp[$ciudad]\">$ciudad</th></tr>";
-                        echo "<tr class=\"tit\"><td class=\"dcha\">Mes</td><td>Temp m&aacutexima</td><td>Temp m&iacutenima</td></tr>";
+                        echo "<table><tr><th colspan=3>$ciudad</th></tr>";
+                        echo "<tr class=\"tit\"><td class=\"dcha\">Mes</td><td>Tm&aacutex</td><td>Tm&iacuten</td></tr>";
                         foreach ($meses as $mes) {
-                            echo "<tr><td name=\"temp[$ciudad][$mes]\" class=\"dcha\">$mes</td>";
+                            echo "<tr><td class=\"dcha\">$mes</td>";
                             foreach ($temps as $temp) {
-                                echo "<td><input type=\"text\" name=\"temp[$ciudad][$mes][$temp]\"></td>";
+                                echo "<td><input type=\"text\" class=\"lil\"name=\"temp[$ciudades][$mes][$temp]\"></td>";
                             }
                             echo "</tr>";
                         }
                         echo "</table>";
                     }
             ?>
-            <div class="centrado"><input type="SUBMIT" value="Enviar"></div>
+            <div class="centrado"><input type="SUBMIT" value="Enviar" class="centrado"></div>
         </form>
     </body>
 </html>
