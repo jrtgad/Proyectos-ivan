5951bd8aee3f50f45966f80206fe6aa1a7dd34ce
2daw <2daw@A101PC1>
2015-10-05 20:32:53 +0200

    Actualizado

--- Temperaturas/php/temperaturas.php
+++ Temperaturas/php/temperaturas.php
@@ -1,7 +1,23 @@
+<html>
+    <head>
+        
+    </head>
+    <body>
 <?php
 
 $temps = $_POST['temp'];
 
+$ciudades = $temps['ciudad'];
+$meses = $temps['ciudad']['mes'];
+$temperaturas = $temps['ciudad']['mes']['temperaturas'];
+    
+foreach ($ciudades as $ciudad => $mes) {
+    echo "$ciudad elll $mes";
+}
+
 
 
 ?>
+
+            </body>
+</html>
