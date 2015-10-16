17644ca5bf8f4f0ce456e6b885f9d48c5ead9247
JRT <jruiztoledog@gmail.com>
2015-10-16 19:26:01 +0200

    viernes2

--- Partidos/index.php
+++ Partidos/index.php
@@ -9,7 +9,7 @@
 
             table   {border: solid 3px red;
                     border-collapse: collapse;
-                    text-align: center;}
+                    margin: 0px auto;}
 
             th      {text-align: center;
                     font: normal bold 2em/1.5 sans-serif;}
@@ -25,28 +25,24 @@
         <div class="centrado">
             <form action="puntua.php" method="POST">
                 <h1>Liga de campeones</h1>
-                <table>
-                    <tr>
-                        <th>Local</th>
-                        <th>Resultado</th>
-                        <th>Visitante</th>
-                    </tr>
+                
                     <?php
                     $equiposLocales = ["Real Madrid", "Manchester United", "AC Milan"];
                     $equiposVisitantes = ["Real Madrid", "Manchester United", "AC Milan"];
-                    
-                    foreach ($equiposLocales as $equipo) {
-                        echo "<tr><td>" . $equipo . "</td>";
-                        echo "<td><input type=text name=resultado></td>";
+                    echo "<table><tr><th colspan=3>Resultado</th></tr>";
+                    foreach ($equiposLocales as $equipoLoc) {
                         foreach ($equiposVisitantes as $equipoVis) {
-                            while ($equipoVis !== $equipo) {
+                            if ($equipoLoc !== $equipoVis) {
+                                echo "<tr><td>" . $equipoLoc . "</td>";
+                                echo "<td><input type=text "
+                                . "name=datos[" . $equipoLoc . "][" . $equipoVis . "][resultado] value=\"1-1\"></td>";
                                 echo "<td>" . $equipoVis . "</td></tr>";
                             }
                         }
                     }
                     ?>
                 </table>
-                <input type="submit" value="Enviar" name="">
+                <input type="submit" value="Enviar">
             </form>
         </div>
     </body>
--- Partidos/puntua.php
+++ Partidos/puntua.php
@@ -2,12 +2,4 @@
 
 $datos = $_POST['datos'];
 
-$local = $datos['local'];
-$visitante = $datos['visitante'];
-$resultadoLargo = $datos['resultado'];
-
-$goles = explode("-", $resultadoLargo);
-
-$golesLocal = $goles[0];
-$golesVisitante = $goles[1];
 ?>
