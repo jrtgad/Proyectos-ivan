a5078914cbfb8e2e35b243997021c1117fdcb7e2
JRT <jruiztoledog@gmail.com>
2015-10-19 21:35:18 +0200

    Lunes

--- 3enRaya/index.php
+++ 3enRaya/index.php
@@ -2,11 +2,13 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Juego de las 3 en raya</title>
+        <link rel="stylesheet" href="../css/style.css">
+        <script src="js/imagen.js"></script>
     </head>
     <body>
         <?php
-        
+            include "vistas/bienvenida.php";
         ?>
     </body>
 </html>
