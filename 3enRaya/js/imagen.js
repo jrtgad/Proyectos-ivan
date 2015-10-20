85c79523c70a49aadcc696840b5c0e097f7478e5
jrtgad <jruiztoledog@gmail.com>
2015-10-20 23:46:52 +0200

    asde

--- 3enRaya/js/imagen.js
+++ 3enRaya/js/imagen.js
@@ -1,14 +1,16 @@
 imgCPU = "<img src=\"../img/CPU.png\" alt=\"x\"/>";
 imgUSER = "<img src=\"../img/USER.png\" alt=\"o\"/>";
 
-imagen = false;
+imagen = true;
 
 function ponerImagen(celda) {    
-    if (celda.childElementCount < 2)  {
-        celda.innerHTML = imgUSER;
-        imagen = true;
-    } else {
-        celda.innerHTML = "";
-        imagen = false;
+    if(imagen) {
+        if (celda.childElementCount < 2)  {
+            celda.innerHTML = imgUSER;
+            imagen = false;
+        } else {
+            celda.innerHTML = "";
+            imagen = true;
+        }
     }
 }
