911c58a90f570bd9456f16092a8ee31940bca9b0
JRT <jruiztoledog@gmail.com>
2015-10-23 20:00:04 +0200

    por finnnnnnn

--- 3enRaya/jugada.php
+++ 3enRaya/jugada.php
@@ -1,44 +1,52 @@
 <?php
-
 function checkWinner($jugada) {
     $aux = "0";
     
-    for($i = 0; $i < 9; $i += 3) {
-        if (isset($jugada[$i]) && 
-            isset($jugada[$i + 1]) &&
-            isset($jugada[$i + 2])) {
-
-                if (($jugada[$i] === $jugada[$i + 1] && $jugada[$i] === $jugada[$i + 2]) || 
-                    ($jugada[$i] === $jugada[$i + 3] && $jugada[$i] === $jugada[$i + 6])) {
-                    $aux = $jugada[$i];
-                } else if ($jugada[2] === $jugada[4] && $jugada[2] === $jugada[6]) {
+    for($i = 0; $i < 7; $i += 3) {
+        if (isset($jugada[$i]) && isset($jugada[$i + 1]) && isset($jugada[$i + 2])) {
+            if ($jugada[$i] === $jugada[$i + 1] && $jugada[$i] === $jugada[$i + 2]) {
+                $aux = $jugada[$i];
+                $i = 11;
+            } else if (isset($jugada[$i]) && isset($jugada[$i + 3]) && isset($jugada[$i + 6])){
+                    if ($jugada[$i] === $jugada[$i + 3] && $jugada[$i] === $jugada[$i + 6]) {
+                $aux = $jugada[$i];
+                $i = 11;
+                    }
+                } else if(isset($jugada[2]) && isset($jugada[4]) && isset($jugada[6])) {
+                if ($jugada[2] === $jugada[4] && $jugada[2] === $jugada[6]) {
                     $aux = $jugada[2];
-                } 
+                } else if(isset($jugada[0]) && isset($jugada[4]) && isset($jugada[8])) {
+                    if ($jugada[0] === $jugada[4] && $jugada[8] === $jugada[0]) {
+                        $aux = $jugada[0];
+                    }
+                }
             }
         }
+    }
     return $aux;
 }
 
 $jugada = $_POST['jugada'];
-//var_dump($jugada);
 
-//Lo primero es comprobar si hay algún ganador, en caso de haberlo
-//mostraremos la vista correspondiente, sino volvemos a mostrar el tablero
-//en la vista continua
-if (checkWinner($jugada) === "0") {
+//Lo primero es comprobar si ha ganado el jugador, de ser así
+//mostraremos la vista correspondiente, 
+//sino generamos la jugada de CPU
+if (checkWinner($jugada) === "1") {
+    include './vistas/ganajugador.php';
+} else if (checkWinner($jugada) === "0") {
     for ($i = 0; $i < 9; $i++) {
         if (!isset($jugada[$i])) {
             $jugada[$i] = "2";
-            if (checkWinner($jugada[$i]) === "2") {
-                include './vistas/ganacpu.php';
-            }
             $i = 11;
+            }
         }
     }
+
+//Una vez jugado CPU, comprobamos si gana, sino,
+//mostramos la vista continua.php para que mueva el jugador
+if (checkWinner($jugada) === "2") {
+    include './vistas/ganacpu.php';
+} else {
     include './vistas/continua.php';
-} else if (checkWinner($jugada) === "1") {
-    include './vistas/ganajugador.php';
-    } else {
-        include './vistas/ganacpu.php';
-    }
+}
 ?>
--- 3enRaya/vistas/ganajugador.php
+++ 3enRaya/vistas/ganajugador.php
@@ -7,9 +7,6 @@
     <body>
         <h1>Enhorabuena! Ha ganado a la CPU (aunque tampoco era muy dificil.... :P)</h1>
         <form action="../index.php" method="POST">
-            <?php 
-                include "tablero.php";
-            ?>
             <input type="SUBMIT" value="Volver a jugar" name="botonreset">
         </form>
     </body>
