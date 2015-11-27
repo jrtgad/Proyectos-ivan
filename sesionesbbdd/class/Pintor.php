e8d6e0245676064f2ad0768c9087d956bcdbe1d3
Javi <jruiztoledog@gmail.com>
2015-11-27 21:28:17 +0100

    viernes

--- sesionesbbdd/class/Pintor.php
+++ sesionesbbdd/class/Pintor.php
@@ -4,10 +4,27 @@
     class Pintor {
         private $id;
         private $nombre;
+        private $colection;
         
-        public static function getPintor() {
+        public function __construct($nombre = null, $id = null) {
+            $this->id = $id;
+            $this->nombre = $nombre;
+        }
+
+        public static function creaPintor($idPintor) {
             $conexion = BD::getConexion();
-            $query = "SELECT nombre from pintores";
+            $query = "SELECT * from pintores where id=:user_fk_pintor";
+            $prepara = $conexion->prepare($query);
+            $prepara->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, "Pintor");
+            $prepara->execute(array(":user_fk_pintor"=>$idPintor));
+            $pintor = $prepara->fetch();
+            $pintor->setColection(Cuadros::creaCuadros($pintor->getId()));
+            return $pintor;
+        }
+        
+        public static function getPintores() {
+            $conexion = BD::getConexion();
+            $query = "SELECT * from pintores";
             $prepara = $conexion->prepare($query);
             
             //No hace falta porque lo voy a recoger todo
@@ -17,18 +34,37 @@
             $pintor = $prepara->fetchAll();
             return $pintor;
         }
-
+        
+        public static function getCuadroAleatorio($idPintor) {
+            $conexion = BD::getConexion();
+            $query = "SELECT imagen from cuadros where pintor_fk = $idPintor";
+            $prepara = $conexion->prepare($query);
+            
+            //No hace falta porque lo voy a recoger todo
+            $prepara->setFetchMode(PDO::FETCH_ASSOC);
+                                //No hace falta porque no hemos metido variables
+            $prepara->execute(array(/*":pintores.id"=>$idPintor*/));
+            $cuadro = $prepara->fetchAll();
+            return $cuadro[rand(0, count($cuadro) - 1)]['imagen'];
+        }
+        
         public function getId() {
             return $this -> id;
         }
         public function setId($id) {
-            $this -> $id = $id;
+            $this -> id = $id;
         }
         public function getNombre() {
             return $this -> pintor;
         }
         public function setNombre($pintor) {
-            $this -> $pintor = $pintor;
+            $this -> pintor = $pintor;
+        }
+        function getColection() {
+            return $this->colection;
+        }
+        function setColection($colection) {
+            $this->colection = $colection;
         }
     }        
 ?>
